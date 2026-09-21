205ec278c6109863a156374f0ac7246ce0b3973d
Çağıl Alsaç <cagilalsac@outlook.com>
2026-09-21 16:22:16 +0300

    Add project files.

---
+++ CORE/Domain/Record.cs
@@ -0,0 +1,28 @@
+﻿namespace CORE.Domain
+{
+    public abstract class Record
+    {
+        //private int id; // field
+
+        //public void setId(int id) // behavior
+        //{
+        //    this.id = id;
+        //}
+
+        //public int getId()
+        //{
+        //    return id;
+        //}
+
+        public int Id { get; set; } // property
+
+        protected Record()
+        {
+        }
+
+        protected Record(int id)
+        {
+            Id = id;
+        }
+    }
+}
