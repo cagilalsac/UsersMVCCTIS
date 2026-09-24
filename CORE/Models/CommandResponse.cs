937632b1c6bda04f622f5d3e024c2e776f9b4452
Çağıl Alsaç <cagilalsac@outlook.com>
2026-09-24 11:44:32 +0300

    CommandResponse and Service classes completed.

---
+++ CORE/Models/CommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using CORE.Domain;
+
+namespace CORE.Models
+{
+    public class CommandResponse : Record
+    {
+        public bool IsSuccessful { get; }
+
+        public string Message { get; }
+
+        public CommandResponse(bool isSuccessful, string message, int id = 0) : base(id)
+        {
+            IsSuccessful = isSuccessful;
+            Message = message;
+        }
+    }
+}
